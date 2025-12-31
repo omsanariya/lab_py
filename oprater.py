266e1ad31a1cefa229c0209aa0e83284788ac3dd
omsanariya <omsanariya571@gmail.com>
2025-12-31 08:04:31 +0530

    Add files via upload

---
+++ oprater.py
@@ -0,0 +1,54 @@
+#assignment oprater
+x = 10
+print(x)
+
+
+#2.urnary minus
+a=5
+print(-a)
+
+
+#3.relational
+p=10
+q=5
+print(p>q)
+print(p==q)
+
+
+#4.logical
+d=True
+e=False
+print(d or e)
+print(d and e)
+print(not d)
+
+
+#6.bitwise
+m = 5
+n = 3
+
+print(m & n)
+print(m | n)
+
+#7.membership
+number = [1,2,3,4,5,6]
+print(2 in number)
+print(5 not in number)
+
+
+#8.identity
+r=10
+u=10
+
+print(r is u)
+print(r is not u)
+
+
+#9.arithmetic
+s=20
+f=10
+
+print(s + f)
+print(s - f)
+print(s * f)
+print(s / f)
