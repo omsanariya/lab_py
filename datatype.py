bf6d099241b14540c2227c250df027117e3982e9
omsanariya <omsanariya571@gmail.com>
2026-01-05 11:22:55 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,26 @@
+#1.int
+x = 15
+print(x)
+#2.str
+a = "omsanariya"
+print(a)
+#3.float
+b = 2.88
+print(b)
+#4.boolen
+p = True
+print(p)
+#5.tuple
+i = ("apple","boll","catch")
+print(i)
+#6.list
+list = ["apple","boll","catch"]
+print(list)
+#7.dict
+d={"name":"om","age":"18"}
+print(d)
+#8.complex
+k = lj
+print(k)
+#9.none 
+a = none
