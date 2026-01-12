34ca1223197456dbb7ad9dfbf492c3efe7b72803
omsanariya <omsanariya571@gmail.com>
2026-01-12 11:43:06 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,45 @@
+# q-1) print("welcome")
+# q-2) print("rajesh kumar \n flat no. 101,sunship apartment \n mg road, sectore 15 \n rajkot, \n pincode : 360004 \n india")
+"""a = 150
+b = 120.50
+
+ q-3) print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)"""
+ 
+""" q-4) r = 5
+area = 3.14159 * r * r
+print(area)"""
+
+""" q-5) p = 11
+q = 12
+r = 20
+intrest = p * q * r / 100
+print("intrest is ",intrest)"""
+
+""" q-6) leng = 10
+width = 20
+perimeter = 2 *(leng + width)
+print("perimeter of rectangle =",perimeter)"""
+
+""" q-7) leng = 10
+width = 20
+perimeter = 2 *(leng + width)
+area = leng * width 
+print("perimeter of rectangle =",perimeter)
+print("area of rectangle =",area)"""
+
+""" q-8) a = 2
+b = 4
+c = 7
+per = a+b+c
+print("primeter of tringle = ",per)"""
+
+""" q-9) slide = 40
+area = slide * slide
+print("area of sqaur = ",area)"""
+
+""" q-10) m = 5
+per = 4 * 5
+print("primeter of squar = ",per)"""
