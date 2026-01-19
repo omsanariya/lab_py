71e0e382acd64abf7bc5713efc6ac7ac97fc418c
omsanariya <omsanariya571@gmail.com>
2026-01-19 11:03:46 +0530

    Add files via upload

---
+++ taskone.py
@@ -0,0 +1,66 @@
+#task 1)
+
+"""p = 10
+q = 2
+r = 11
+
+intrest = p*q*r/100
+
+print(intrest)"""
+
+#task 2)
+
+"""a = 10
+b = 20
+print("max value is ",max(a,b))"""
+
+#task 3)
+
+print("1 2 3 4 5")
+
+#taske 4)
+
+"""ip = "helllooo"
+length = len(ip)
+
+print("length of string is",length)"""
+
+#task 5)
+
+"""print("welcome to py")"""
+
+#task 6)
+
+"""my = "Python"
+first = my[0]
+print(first)"""
+
+#task 7)
+
+"""my = "Python"
+first = my[-1]
+print(first)"""
+
+#task 8)
+
+"""number = 20
+if number > 0:
+    print("The number is positive.")
+elif number < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")"""
+
+
+#task 9)
+
+"""x=10
+y=10
+z=10
+print(x+y+z)"""
+
+#task 10)
+
+"""age = int(input("Enter your age :"))
+
+print("your age is ",age)"""
