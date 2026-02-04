bde8245f2f1013fc923680740e9698da66ce8850
omsanariya <omsanariya571@gmail.com>
2026-02-04 09:06:01 +0530

    Add files via upload

---
+++ iO.py
@@ -0,0 +1,48 @@
+#int
+a =5
+b =10
+print(a)
+print(b)
+
+#float
+x=3.999
+y=5.93
+print(x)
+print(y)
+
+#boolen
+p= True
+n= False
+print(p)
+print(n)
+
+#BOOL
+name="string"
+name2="com"
+print(name)
+print(name2)
+
+#output
+
+ #INT
+age=int(input("Enter value"))
+print(age)
+number=int(input("Enter value"))
+print(number)
+
+#float
+mark=float(input("Enter ypur mark"))
+print("your mark is ",mark)
+money=float(input("Enter income"))
+print("your balnce is ",money)
+
+#bool
+my=bool(input("Enter t or f ="))
+print(my)
+gg=bool(input("Enter t or f ="))
+print(gg)
+
+name = input("enter your name =")
+print(name)
+name1 = input("enter your surname=")
+print(name + name1) 
