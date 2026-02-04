bcbdf4d00e6f4faced9755f4c452d4f5b6fa702d
omsanariya <omsanariya571@gmail.com>
2026-02-04 09:08:57 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,39 @@
+#voter id  
+"""age = int(input("Enter your age"))
+if age>18:
+    print("yes you are eligible")
+else:
+    print("No you are not eligible") """   
+
+#garde
+"""marks = int(input("Enter your mark"))
+if marks>=90:
+    print("you get a grade")
+elif marks>=70:
+    print("You get b grade")
+else:
+    print("you get c grade")""" 
+
+# odd or even
+"""num = int(input("Enter number"))
+if num%2==0:
+    print("EVEN")
+else:
+    print("ODD")"""
+    
+#positive or negative
+"""number = int(input("Enter number"))
+if number==0:
+    print("Number is zero")
+elif number>0:
+    print("Number is positive")
+else:
+    print("Number is negative")"""
+    
+#licenc  
+"""age = int(input("Enter your age"))
+if age>18:
+    print("yes you are eligible")
+else:
+    print("No you are not eligible") """        
+                    
