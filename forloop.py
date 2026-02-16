5deebdfe581081f6d913c928a93280dea347adab
omsanariya <omsanariya571@gmail.com>
2026-02-16 11:13:32 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,41 @@
+#print 1 to 5
+'''for i in range(1,6):
+    print(i)'''
+    
+#print hello for 3 time    
+'''for i in range(3):
+  print("HEllo world")'''
+  
+#print 1 to 10       
+'''for i in range(1,11):
+    print(i)'''
+    
+#print even number from 1 to 20
+'''for i in range(1,21):
+    if(i%2==0):
+        print(i)'''    
+
+#print odd number from to 15        
+'''for i in range(1,16):
+    if(i%2!=0):
+        print(i)'''
+
+#print table of 5
+'''for i in range (1,11):
+  print("5 X",i,"=",5*i)'''
+ 
+#print letter of string
+'''name = "hello"
+for letter in name:
+    print(letter)'''
+
+#print sum of 1 to 5
+'''sum=0
+for i in range(1,6):
+       sum = sum+i
+       print(sum)''' 
+       
+#print list element
+'''number =[10,20,30,45]
+for n in number:
+    print(n)'''             
