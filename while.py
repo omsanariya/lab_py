80583afe160ee439d731cb3aee5b6d471d72b9e1
omsanariya <omsanariya571@gmail.com>
2026-02-23 11:03:49 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,19 @@
+'''i = 1
+while i <= 10:
+    print(i)
+    i = i+1'''
+    
+'''n = int(input("Enter number ="))    
+sum =0
+i =1
+while i <= n:
+      sum = sum + i
+      i = i + 1
+print(sum)'''
+
+
+'''num = int(input("Enter number you want its table"))
+i = 1
+while i <= 10:
+    print(num,"X",i,"=",num*i)
+    i = i+1'''
