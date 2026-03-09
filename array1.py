d1f38b40b068b25c047d52cb9862046d097aa36a
omsanariya <omsanariya571@gmail.com>
2026-03-09 11:28:37 +0530

    Add files via upload

---
+++ array1.py
@@ -0,0 +1,30 @@
+from array import array
+'''arr = array('i',[10,20,30,40,50])
+print(arr)'''
+
+'''arr = array('i',[10,20,30])
+arr.append(50)
+print(arr)'''
+
+'''arr = array('i',[10,20,30])
+arr.insert(2,50)
+print(arr)'''
+
+'''arr = array('i',[10,20,30,50,60,50])
+arr.remove(50)
+print(arr)'''
+
+'''arr = array('i',[10,20,30,50,6,50])
+print(len(arr))'''
+
+'''arr = array('i',[10,20,30,50,60,50])
+x=arr.count(50)
+print(x)'''
+
+'''arr = array('i',[10,20,30,50,60,50])
+y = arr.index(50)
+print(y)'''
+
+'''arr = array('i',[10,20,30,50,60,50])
+arr.reverse()
+print(arr)'''
