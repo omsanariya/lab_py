b04cff2ad82c9bb113ecd6480f26e9b7cccbc431
omsanariya <omsanariya571@gmail.com>
2026-03-09 11:29:12 +0530

    Add files via upload

---
+++ array2.py
@@ -0,0 +1,22 @@
+from array import array
+#1) positive indexing
+'''arr = array('i',[10,20,30,50,60,50])
+print(arr[0])
+print(arr[4])'''
+
+#2) negative indexing
+'''arr = array('i',[10,20,30,50,60,50])
+print(arr[-1])
+print(arr[-3])
+print(arr[-6])'''
+
+#3)modify at specific index
+'''arr = array('i',[10,20,30,50,60,50])
+arr[1]=25
+print(arr)'''
+
+#4)error of indexing
+'''arr = array('i',[10,20,30,50,60,50])
+print(arr[8])'''
+#output
+ #array index out of range
