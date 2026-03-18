aa9178559365dfb0d14720db38a52d4a9d64e3a0
omsanariya <omsanariya571@gmail.com>
2026-03-18 08:20:34 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,55 @@
+'''def add(a,b):
+    print(a)
+    print(b)
+    return a + b
+result = add(5,8)
+print(result)'''
+ 
+'''def student_info(name,age):
+    print(name)
+    print(age)
+student_info("om",43)'''
+
+'''def s_i(p,r,n):
+    si=(p*r*n)/100
+    print(si)
+s_i(1000,5,5)
+s_i(45435,7,8)'''
+
+'''def ar_cir(r):
+    area=3.14*r*r
+    print(area)
+ar_cir(10)
+ar_cir(45)'''
+
+'''def pon(n):
+    if(n==0):
+        print("number is zero")
+    elif(n<0):
+        print("Number is negative")
+    else:
+        print("Number is positive")
+pon(-3)
+pon(4)
+pon(0)'''
+        
+'''def odd_even(no):
+    if(no%2==0):
+        print("Number is even ")
+    else:
+        print("Number is odd")
+odd_even(4)
+odd_even(5)'''
+
+'''def arith(a,b):
+    add=a+b
+    print("addition is =",add)
+    sub=a-b
+    print("subtraction is =",sub)
+    mul=a*b
+    print("Multipliction is =",mul)
+    div=a/b
+    print("division is =",div)
+arith(10,5)'''
+
+                       
