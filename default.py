0eb5ef45090d51fb90125d2830d8e1338f8b878a
omsanariya <omsanariya571@gmail.com>
2026-03-23 10:55:01 +0530

    Add files via upload

---
+++ default.py
@@ -0,0 +1,10 @@
+'''def na(name="vraj"):
+    print("name is ",name)
+na("om")
+na()'''
+
+'''def add(a,b=1):
+    result=a+b
+    print(result)
+add(20,30)
+add(2)'''
---
+++ keyfun.py
@@ -0,0 +1,4 @@
+def inter(p:float,r:float,n:float):
+    intrest=(p*r*n)/100
+    print("simple intrest =",intrest)
+inter(p=20.40232,r=20.777,n=10)\
